aef2c99c02ab922f767d23d62d9930d908450228
butin <butin@g3250.net.fpt>
2019-08-27 17:04:49 +0700

    fix

--- ggsheet.js
+++ ggsheet.js
@@ -3,7 +3,7 @@ $(function () {
 	var jsonUrl = 'https://spreadsheets.google.com/feeds/list/'+sheet+'/public/full?alt=json';
 	$.getJSON(jsonUrl, function(data){
 		var entry = data.feed.entry;
-		console.log(entry.length);
+		//console.log(entry.length);
 		$.each(entry, function (i,e) {
 			$('.blog-main').prepend('<div id="entry'+i+'" class="blog-post hidden"></div>');
 			$('#entry'+i).append('<h2 class="blog-post-title"><a href="#block'+i+'" aria-controls="block'+i+'" data-toggle="collapse" aria-expanded="false">'+e.gsx$title.$t+'</a></h2>');
@@ -14,29 +14,30 @@ $(function () {
 		});
 	})//getJSON
 
-	
-});//doc end
-$(function () {
 	$('#txtSeeMore').summernote({
 		placeholder: 'Write here ...',
 		height: 100,
 		tabsize: 2
-	  });
-	  //$('#my-summernote2').summernote({airMode: true,placeholder:'Try the airmode'});
-});
-$(window).scroll(function() {
-	if($(window).scrollTop() + $(window).height() == $(document).height()) {
-		loadMore();
-	}
-});
+	});
+	//$('#my-summernote2').summernote({airMode: true,placeholder:'Try the airmode'});
+	guid = (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+	//alert(guid);
+	$('#guid').val(guid);
+});//doc end
+function S4() {
+	return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
 
 function formOK() {
 	if (confirm("Tốt lắm! Đợi duyệt....")) {
 		window.location.href=window.location.href;
-	}else{
-		alert('nhớ clear');
 	}
 }
 function loadMore(){
 	$(".blog-main .hidden").slice(0,2).removeClass("hidden");
 }
+$(window).scroll(function() {
+	if($(window).scrollTop() + $(window).height() == $(document).height()) {
+		loadMore();
+	}
+});
